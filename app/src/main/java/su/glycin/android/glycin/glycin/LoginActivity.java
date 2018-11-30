f22741eff8510af06c7ea022e5ef8d7805e6c984
Igor Korovchenko <korovchenko@phys.vsu.ru>
2018-11-30 11:16:31 +0300

    Initialization of the fields.

--- app/src/main/java/su/glycin/android/glycin/glycin/LoginActivity.java
+++ app/src/main/java/su/glycin/android/glycin/glycin/LoginActivity.java
@@ -2,12 +2,39 @@ package su.glycin.android.glycin.glycin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * Email text field on the Activity
+     */
+    private EditText emailEditText;
+
+    /**
+     * Password text field on the Activity
+     */
+    private EditText passwordEditText;
+
+    /**
+     * Log in button on the Activity
+     */
+    private Button loginButton;
+
+    /**
+     * Overridden standard activity lifecycle method
+     *
+     * @param savedInstanceState bundle of the App
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialization of the Activity's fields
+        this.emailEditText = findViewById(R.id.login_email_edit_text);
+        this.passwordEditText = findViewById(R.id.login_password_edit_text);
+        this.loginButton = findViewById(R.id.login_button_login);
     }
 }
